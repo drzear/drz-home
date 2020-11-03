a68e1bef661625d381bef33a396708d84891ea03
daze269279 <david.zear@radissonhotels.com>
2020-11-03 15:08:51 +0100

    :construction: contact icons/links

--- src/Components/Pages/Contact/Contact.tsx
+++ src/Components/Pages/Contact/Contact.tsx
@@ -1,13 +1,62 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Media, Row } from "react-bootstrap";
 import "./Contact.css";
 
 function Contact() {
     return (
         <>
             <Container className="contact-container">
-                <Row>Linkedin</Row>
-                <Row>Github</Row>
+                <br />
+                <Row className="justify-content-center">
+                    <Media>
+                        <a
+                            href="https://www.linkedin.com/in/david-zear-68244910/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                src={require("../../../Images/Logos/linkedin.png")}
+                                className="align-self-center contact-logo-linkedin"
+                                alt="LinkedIn"
+                                title="LinkedIn"
+                            />
+                        </a>
+                    </Media>
+                </Row>
+                <br />
+                <Row className="justify-content-center">
+                    <Media>
+                        <a
+                            href="https://github.com/drzear"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                src={require("../../../Images/Logos/github.png")}
+                                className="align-self-center contact-logo"
+                                alt="Github"
+                                title="Github"
+                            />
+                        </a>
+                    </Media>
+                </Row>
+                <br />
+                <Row className="justify-content-center">
+                    <Media>
+                        <a
+                            href="mailto:drzear@gmail.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                src={require("../../../Images/Logos/gmail.png")}
+                                className="align-self-center contact-logo"
+                                alt="Gmail"
+                                title="Gmail"
+                            />
+                        </a>
+                    </Media>
+                </Row>
             </Container>
         </>
     );
